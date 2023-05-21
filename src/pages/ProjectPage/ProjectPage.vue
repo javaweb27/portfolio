0dbfd1b0fcf9ed739616179cf3198224fef16b91
javier <devjavicont@gmail.com>
2023-05-21 11:42:46 -0400

    fix: menu button isn't always at the left botton because the ProjectPage doesn't have a min-height

--- src/pages/ProjectPage/ProjectPage.vue
+++ src/pages/ProjectPage/ProjectPage.vue
@@ -5,7 +5,7 @@ import SideBar from '@/features/projects/layouts/SideBar'
 
 <template>
   <SideBar>
-    <div class="px-[calc(4vw-6px)] py-7 h-full">
+    <div class="px-[calc(4vw-6px)] py-7 min-h-full">
       <h1 class="text-4xl mb-7 text-center">My projects</h1>
       <ProjectsList />
     </div>
